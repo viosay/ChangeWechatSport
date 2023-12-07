0c4cfb60d3e080418acf3e239daa842195fa9e76
viosay <530534635@qq.com>
2023-12-07 01:16:15 +0000

    v2ray sign

---
+++ v2ray.py
@@ -0,0 +1,64 @@
+import base64
+import hashlib
+import hmac
+import urllib
+
+import requests
+
+
+def v2ray_sign():
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+        'Cookie': v2ray_cookie
+    }
+    sign_url = "https://go.runba.cyou/user/checkin"
+    return_response = requests.post(sign_url, headers=headers, allow_redirects=False)
+    response_text = return_response.text.encode('utf-8').decode('unicode_escape')
+    print(response_text)
+    ding_push(response_text)
+
+
+headers = {
+    'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 9; MI 6 MIUI/20.6.18)'
+}
+
+
+def get_time():
+    url = 'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp'
+    response = requests.get(url, headers=headers).json()
+    t = response['data']['t']
+    return t
+
+
+def ding_push(content):
+    timestamp = get_time()
+    secret_enc = ding_secret.encode('utf-8')
+    string_to_sign = '{}\n{}'.format(timestamp, ding_secret)
+    string_to_sign_enc = string_to_sign.encode('utf-8')
+    hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+    sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+    url = f"https://oapi.dingtalk.com/robot/send?access_token={ding_access_token}&timestamp={timestamp}&sign={sign}"
+    headers = {
+        "Content-Type": "application/json;charset=UTF-8",
+    }
+    data = ("{\"at\":{\"isAtAll\":true},\"msgtype\":\"text\",\"text\":{\"content\":\"" + content + "\"}}").encode(
+        'utf-8')
+    response = requests.post(url, data=data, headers=headers).json()
+    if response["errcode"] != 0:
+        print("钉钉推送失败")
+
+
+def main():
+    v2ray_sign()
+
+
+def main_handler(event, context):
+    return main()
+
+
+if __name__ == '__main__':
+    ding_access_token = ''
+    ding_secret = ''
+    v2ray_cookie = ''
+    main()
