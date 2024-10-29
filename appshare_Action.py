d6b9e959f3f2bd84d8a475b744932474325749e6
viosay <windclear123@gmail.com>
2024-10-29 01:06:53 +0000

    获取本地时间戳

--- appshare_Action.py
+++ appshare_Action.py
@@ -67,13 +67,14 @@ headers = {
 
 def get_time():
     # url = 'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp'
-    url = 'https://worldtimeapi.org/api/timezone/Asia/ShangHai'
-    response = requests.get(url, headers=headers).json()
-    # t = response['data']['t']
-    utc_time_str = response['utc_datetime']
-    utc_time = datetime.fromisoformat(utc_time_str.replace("Z", "+00:00"))
-    t = int(utc_time.timestamp() * 1000)
-    return t
+    # url = 'https://worldtimeapi.org/api/timezone/Asia/ShangHai'
+    # response = requests.get(url, headers=headers).json()
+    # # t = response['data']['t']
+    # utc_time_str = response['utc_datetime']
+    # utc_time = datetime.fromisoformat(utc_time_str.replace("Z", "+00:00"))
+    # t = int(utc_time.timestamp() * 1000)
+    # return t
+    return int(datetime.now().timestamp() * 1000)
 
 
 def ding_push(content):
